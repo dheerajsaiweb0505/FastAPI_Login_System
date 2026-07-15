907871a9ddda2eb73b07d426e477fe661fc5b7b9
dheerajsaiweb0505 <your-dheerajsaiemmadishetti@gmail.com>
2026-07-15 10:17:00 +0530

    Configure database and update gitignore

--- app/database.py
+++ app/database.py
@@ -12,9 +12,7 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
+    autocommit=False,autoflush=False, bind=engine
 )
 
 Base = declarative_base()
